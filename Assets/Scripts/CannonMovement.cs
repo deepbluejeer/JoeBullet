fb54136e213ae88eaab6f58fbbbb6f867cea0e48
deepbluejeer <xellotath1@gmail.com>
2016-05-27 17:18:15 -0300

    The Game

---
+++ Assets/Scripts/CannonMovement.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonMovement : MonoBehaviour
+{
+    public GameObject position1;
+    public GameObject position2;
+    public float rotation;
+    GameObject destination;
+
+    public bool isRotating;
+    bool hasPlayer;
+
+    public int cannonSpeed;
+
+    private Rigidbody cannon;
+
+    public enum Axial : int { RIGHT=0, LEFT=180, UP=90, DOWN=270};
+    public Axial initialAxis;
+    public Axial finalAxis;
+
+    // Use this for initialization
+    void Start()
+    {
+        cannon = GetComponent<Rigidbody>();
+        destination = position1;
+    }
+
+    void cannonFacing(Axial currentAxis)
+    {
+        Axial axis = currentAxis;
+        Quaternion actualRotation;
+        actualRotation = transform.rotation;
+
+        switch (axis)
+        {
+            case Axial.RIGHT:
+                transform.rotation = Quaternion.Lerp(actualRotation, Quaternion.Euler(0, 0, (float)Axial.RIGHT), 10f * Time.deltaTime);
+                break;
+
+            case Axial.LEFT:
+                transform.rotation = Quaternion.Lerp(actualRotation, Quaternion.Euler(0, 0, (float)Axial.LEFT), 10f * Time.deltaTime);
+                break;
+
+            case Axial.UP:
+                transform.rotation = Quaternion.Lerp(actualRotation, Quaternion.Euler(0, 0, (float)Axial.UP), 10f * Time.deltaTime);
+                break;
+
+            case Axial.DOWN:
+                transform.rotation = Quaternion.Lerp(actualRotation, Quaternion.Euler(0, 0, (float)Axial.DOWN), 10f * Time.deltaTime);
+                break;
+        }
+    }
+
+    void waitingPlayer()
+    {
+        //The cannon should face the direction the player is (i.e. left, up, down). When the player gets inside the cannon, it should rotate to the correct angle.
+        if (!hasPlayer && !isRotating)
+            cannonFacing(initialAxis);
+        else if (hasPlayer && !isRotating)
+            cannonFacing(finalAxis);
+    }
+
+    void MoveCannon()
+    {
+        //Move cannon between two predefined positions.
+        if (destination != null)
+        {
+            Vector3 direction = (destination.transform.position - transform.position).normalized;
+            cannon.MovePosition(transform.position + direction * cannonSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(destination.transform.position, transform.position) <= 0.2f)
+                destination = (destination != position1) ? position1 : position2;
+        }
+    }
+
+    void rotateCannon(float rot)
+    {
+
+
+        if (rot == 360)
+            transform.Rotate(Vector3.forward * 360.0f * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        waitingPlayer();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveCannon();
+        rotateCannon(rotation);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            hasPlayer = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float timeToActivateGravity;
+    public int cannonPower;
+    GameObject currentCannon;
+    GameObject firePoint;
+    Rigidbody player;
+    bool insideCannon;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+        insideCannon = false;
+    }
+
+    void Update()
+    {
+        if (currentCannon != null)
+        {
+            player.transform.position = firePoint.transform.position;
+            player.transform.rotation = currentCannon.transform.rotation;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0) && insideCannon)
+            StartCoroutine(fireCannon());
+    }
+
+    void enterCannon()
+    {
+        insideCannon = true;
+        this.GetComponent<Renderer>().enabled = false;
+        this.GetComponent<Collider>().enabled = false;
+        player.velocity = Vector3.zero;
+        player.angularVelocity = Vector3.zero;
+        player.useGravity = false;
+    }
+
+    IEnumerator fireCannon()
+    {
+        if (insideCannon)
+        {
+            insideCannon = false;
+            this.GetComponent<Renderer>().enabled = true;
+            this.GetComponent<Collider>().enabled = true;
+            player.AddForce(transform.right * cannonPower, ForceMode.VelocityChange);
+            currentCannon = null;
+            firePoint = null;
+
+            yield return new WaitForSeconds(timeToActivateGravity);
+
+            player.useGravity = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Cannon")
+        {
+            currentCannon = other.gameObject;
+            firePoint = other.transform.GetChild(0).gameObject;
+            enterCannon();
+        }
+    }
+}
